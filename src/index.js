39d7641e0c75d8ed311ee69754b03991571131dc
Paul Ayuk <ayukpaul1@gmail.com>
2019-05-08 19:01:16 +0100

    react boilerplate setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Jobs in Africa';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+
+
+module.hot.accept();
